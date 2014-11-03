00477ac3bf06788d583ad2ecbab6bffb80242dd2
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2014-11-04 01:32:45 +0900

    reduce利用版を作成

---
+++ rpn_calculator3.py
@@ -0,0 +1,33 @@
+# 逆ポーランド記法電卓 (関数型スタイル2)
+
+from functools import reduce
+from operator import add, sub, mul, truediv
+
+_OPERATORS = {
+    "+": add,
+    "-": sub,
+    "*": mul,
+    "/": truediv
+}
+
+
+def calculate(rpn):
+    def calc(stack, x):
+        if x.isdigit():
+            return [int(x)] + stack
+        else:
+            return [_ope_fn(x)(stack[1], stack[0])] + stack[2:]
+    return reduce(calc, rpn.split(), [])[0]
+
+
+def _ope_fn(ope):
+    if ope in _OPERATORS:
+        return _OPERATORS[ope]
+    else:
+        raise ValueError("unsupported operator '{0}' is used".format(ope))
+
+
+if __name__ == "__main__":
+    # 利用例
+    rpn = "1 2 + 3 / 4 - 5 *"
+    print(rpn, "\n = ", calculate(rpn))
