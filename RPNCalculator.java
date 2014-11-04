5b7c1c207cc222a9c32ab50ba750c97cd5313be2
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2014-11-04 23:56:58 +0900

    ロジックを実装

--- RPNCalculator.java
+++ RPNCalculator.java
@@ -1,11 +1,44 @@
 // 逆ポーランド記法電卓 (手続き型スタイル)
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
 class RPNCalculator {
   public static double calculate(String rpn) {
-    // TODO: ロジックを実装
+    String[] expr = rpn.split("\\s+");
+    Deque<Double> stack = new ArrayDeque<>();
+    for (String elem : expr) {
+      if (NumberUtils.isDigits(elem)) {
+        stack.addFirst(Double.parseDouble(elem));
+      } else {
+        double res = opeFn(elem, stack.removeFirst(), stack.removeFirst());
+        stack.addFirst(res);
+      }
+    }
+    return stack.removeFirst();
+  }
+
+  private static double opeFn(String ope, double x, double y) {
+    switch (ope) {
+      case "+":
+        return y + x;
+      case "-":
+        return y - x;
+      case "*":
+        return y * x;
+      case "/":
+        return y / x;
+      default:
+        throw new UnsupportedOperationException(
+          "unsupported operator '" + ope + "' is used");
+    }
   }
 
   public static void main(String[] args) {
-
+    // 利用例
+    String rpn = "1 2 + 3 / 4 - 5 *";
+    System.out.println(rpn + "\n = " + calculate(rpn));
   }
 }
