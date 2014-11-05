92096cd8e00d7497c4816bf6f25235081dababe3
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2014-11-06 02:01:32 +0900

    不要なimport文を削除

--- RPNCalculator3.java
+++ RPNCalculator3.java
@@ -5,7 +5,6 @@ import java.util.ArrayDeque;
 import java.util.Collections;
 import java.util.Deque;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
