2a9fb25cf57b4e25c137449e05b383fc03890470
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2015-01-16 00:22:57 +0900

    ロジックを実装

--- rpn_calculator.rb
+++ rpn_calculator.rb
@@ -2,24 +2,39 @@
 
 module RPNCalculator
   OPERATORS = {
-    "+" => :+,
-    "-" => :-,
-    "*" => :*,
-    "/" => :/
+    "+" => ->(x, y) { y + x },
+    "-" => ->(x, y) { y - x },
+    "*" => ->(x, y) { y * x },
+    "/" => ->(x, y) { y.fdiv(x) }
   }
 
   module_function
 
   def calculate(rpn)
-    # TODO: ロジックを実装
+    expr = rpn.split
+    stack = []
+    expr.each do |elem|
+      if elem  =~ /^\d+$/
+        stack.push(elem.to_i)
+      else
+        res = ope_fn(elem).call(stack.pop, stack.pop)
+        stack.push(res)
+      end
+    end
+    stack.pop
   end
 
   def ope_fn(ope)
-
+    if OPERATORS.include?(ope)
+      OPERATORS[ope]
+    else
+      raise NameError, "unsupported operator '#{ope}' is used"
+    end
   end
 end
 
 if __FILE__ == $0
-  # 実行例
-
+  # 利用例
+  rpn = "1 2 + 3 / 4 - 5 *"
+  print rpn, "\n = ", RPNCalculator.calculate(rpn), "\n"
 end
