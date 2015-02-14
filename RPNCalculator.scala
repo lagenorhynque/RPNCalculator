031d52ce00e80265445663b7bc45fc310b6cd388
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2015-02-15 02:04:01 +0900

    新規作成

---
+++ RPNCalculator.scala
@@ -0,0 +1,41 @@
+// 逆ポーランド記法電卓 (関数型スタイル)
+
+import scala.annotation.tailrec
+
+object RPNCalculator {
+  def calculate(rpn: String): Double = {
+    @tailrec
+    def calc(stack: List[Double], expr: List[String]): Double = {
+      expr match {
+        case Nil =>  stack.head
+        case x :: xs =>
+          if (x.forall { _.isDigit }) {
+            calc(x.toDouble :: stack, xs)
+          } else {
+            stack match {
+              case y1 :: y2 :: ys =>
+                calc(opeFn(x)(y2, y1) :: ys, xs)
+            }
+          }
+      }
+    }
+    val expr = rpn.split("""\s+""").toList
+    calc(List(), expr)
+  }
+
+  private def opeFn(ope: String): (Double, Double) => Double = {
+    ope match {
+      case "+" => _ + _
+      case "-" => _ - _
+      case "*" => _ * _
+      case "/" => _ / _
+      case _ => throw new UnsupportedOperationException(s"unsupported operator '${ope}' is used")
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    // 利用例
+    val rpn = "1 2 + 3 / 4 - 5 *"
+    println(s"${rpn}\n = ${calculate(rpn)}")
+  }
+}
---
+++ RPNCalculator2.scala
@@ -0,0 +1,34 @@
+// 逆ポーランド記法電卓 (関数型スタイル2)
+
+object RPNCalculator2 {
+  def calculate(rpn: String): Double = {
+    def calc(stack: List[Double], x: String): List[Double] = {
+      if (x.forall { _.isDigit }) {
+        x.toDouble :: stack
+      } else {
+        stack match {
+          case y1 :: y2 :: ys =>
+            opeFn(x)(y2, y1) :: ys
+        }
+      }
+    }
+    val expr = rpn.split("""\s+""").toList: List[String]
+    expr.foldLeft(List(): List[Double])(calc).head
+  }
+
+  private def opeFn(ope: String): (Double, Double) => Double = {
+    ope match {
+      case "+" => _ + _
+      case "-" => _ - _
+      case "*" => _ * _
+      case "/" => _ / _
+      case _ => throw new UnsupportedOperationException(s"unsupported operator '${ope}' is used")
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    // 利用例
+    val rpn = "1 2 + 3 / 4 - 5 *"
+    println(s"${rpn}\n = ${calculate(rpn)}")
+  }
+}
